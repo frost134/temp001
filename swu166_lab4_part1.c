b03ea2dfa5859d0fc935050df60bc981047692c9
Zinxp <41328957+Zinxp97@users.noreply.github.com>
2019-01-17 13:26:43 -0800

    Update swu166_lab4_part1.c

--- swu166_lab4_part1.c
+++ swu166_lab4_part1.c
@@ -1,53 +1,42 @@
 /*	Partner(s) Name & E-mail:
-
  *  Xuanpeng Zhao & xuanpengzhao97@gmail.com
-
  *  Shuang Wu & frost134@live.com
-
  *	Lab Section: 022 
-
  *	Assignment: Lab 3  Exercise 4
-
  *	Exercise Description: 
-
  *	I acknowledge all content contained herein, excluding
-
     template or example code, is my own original work.
-
  */
 
 #include <avr/io.h>
-
-enum States{Start,B0off_p,B0off_r,B0on_p, B0on_r} state;
+enum States{Start, B1_on, B1_hold, B0_on, B0_hold} state;
 void Tick()
 {
 	switch(state)
 	{
-		case Start : state = B0on_r; break;
-		case B0on_r : if(PINA&0x01){state = B0off_p;}; break;
-		case B0off_p : if(!(PINA&0x01)){state = B0off_r;}; break;
-		case B0off_r : if(PINA&0x01){state = B0on_p;}; break;
-		case B0on_p : if(!(PINA&0x01)){state = B0on_r;}; break;
+		case Start:		state = B0_hold; break;
+		case B0_hold:	state = PINA & 0x01 ? B1_on:B0_hold; break;
+		case B1_on:		state = PINA & 0x01 ? B1_on:B1_hold; break;
+		case B1_hold:	state = PINA & 0x01 ? B0_on:B1_hold; break;
+		case B0_on:		state = PINA & 0x01 ? B0_on:B0_hold; break;
 	}
 	switch(state)
 	{
-		case Start : PORTB = 0x01; break;
-		case B0on_r : PORTB = 0x01; break;
-		case B0off_p : PORTB = 0x02; break;
-		case B0off_r : PORTB = 0x02; break;
-		case B0on_p : PORTB = 0x01;break;
+		case Start:	break;
+		case B0_hold:	PORTB = 0x01; break;
+		case B1_on:		PORTB = 0x02; break;
+		case B1_hold:	PORTB = 0x02; break;
+		case B0_on:		PORTB = 0x01; break;
 	}
 }
 
-
 int main(void)
 {
-    /* Replace with your application code */
 	DDRA = 0x00; PORTA = 0x00;
 	DDRB = 0xff; PORTB = 0x00;
+	state = Start;
     while (1) 
     {
 		Tick();
     }
 }
-
