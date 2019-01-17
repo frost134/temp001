862bdf60b43a861036619dc825b7ac141990d369
frost <frost134@live.com>
2019-01-17 14:41:18 -0800

    Add files via upload

---
+++ swu166_lab4_part2.c
@@ -0,0 +1,84 @@
+/*	Partner(s) Name & E-mail:
+
+ *  Xuanpeng Zhao & xuanpengzhao97@gmail.com
+
+ *  Shuang Wu & frost134@live.com
+
+ *	Lab Section: 022 
+
+ *	Assignment: Lab 4  Exercise 2
+
+ *	Exercise Description: 
+
+ *	I acknowledge all content contained herein, excluding
+
+    template or example code, is my own original work.
+
+ */
+
+#include <avr/io.h>
+
+enum States{Start,reset,plus,minus,wait} state;
+int flag = 0;
+void Tick()
+{
+
+	switch(state)
+	{
+		case Start :if ((PINA&0x01)&&(!(PINA&0x02)))
+		{
+			state = plus;
+		}
+		else if ((!(PINA&0x01))&&(PINA&0x02))
+		{
+			state = minus;
+		}
+		else if((PINA&0x01)&&(PINA&0x02))
+		{
+			state = reset;
+		}
+		else {}
+		break;
+		case wait : if ((PINA&0x01)&&(!(PINA&0x02)))
+		{
+			if(flag == 0){state = plus;}
+		}
+		else if ((!(PINA&0x01))&&(PINA&0x02))
+		{
+			if(flag == 0){state = minus;}
+		}
+		else if((PINA&0x01)&&(PINA&0x02))
+		{
+			state = reset;
+		}
+		else if((!(PINA&0x01))&&(!(PINA&0x02)))
+		{flag = 0;}
+		else {}
+		break;
+		case plus : state = wait; break;
+		case minus : state = wait; break;
+		case reset : state = wait; break;
+	}
+	switch(state)
+	{
+		case Start : PORTD = 0x07; break;
+		case wait : break;
+		case plus : if (PIND == 0x09){flag = 1;}else{PORTD = PIND +1; flag = 1;} break;
+		case minus : if (PIND == 0x00){flag = 1;}else{PORTD = PIND -1; flag = 1;} break;
+		case reset : PORTD = 0x00;break;
+	}
+}
+
+
+int main(void)
+{
+    /* Replace with your application code */
+	DDRA = 0x00; PORTA = 0x00;
+	DDRD = 0xff; PORTD = 0x00;
+	state = Start;
+    while (1) 
+    {
+		Tick();
+    }
+}
+
